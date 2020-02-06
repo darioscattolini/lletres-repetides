60c0f248781bc3ff21f429c7ebd8d578b3658386
Darío Scattolini <dario.scattolini@gmail.com>
2020-02-06 21:15:50 +0100

    Fase 4

---
+++ fase4.js
@@ -0,0 +1,6 @@
+function fase4() {
+    const myName = "Dario";
+    const mySurname = "Scattolini";
+    const myFullName = [...myName.split(""), " ", ...mySurname.split("")];
+    console.log(myFullName);
+}
